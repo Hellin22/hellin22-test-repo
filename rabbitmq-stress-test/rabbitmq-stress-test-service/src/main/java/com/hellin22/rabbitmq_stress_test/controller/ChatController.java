37d8b1608ef6ab71af323c1e88b7cb03070b8f24
백경석\seok <dno06117@naver.com>
2025-07-01 17:27:44 +0900

    :chart_with_upwards_trend: 롬복 주석 추가

--- rabbitmq-stress-test/rabbitmq-stress-test-service/src/main/java/com/hellin22/rabbitmq_stress_test/controller/ChatController.java
+++ rabbitmq-stress-test/rabbitmq-stress-test-service/src/main/java/com/hellin22/rabbitmq_stress_test/controller/ChatController.java
@@ -1,13 +1,16 @@
 package com.hellin22.rabbitmq_stress_test.controller;
 
 import com.hellin22.rabbitmq_stress_test.dto.ChatMessage;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
+import org.springframework.messaging.handler.annotation.MessageExceptionHandler;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 
 @Controller
+@Slf4j
 public class ChatController {
 
     private final SimpMessagingTemplate messagingTemplate;
@@ -19,8 +22,12 @@ public class ChatController {
 
     @MessageMapping("/chat.{roomId}") // /app/chat.{roomId}
     public void send(@DestinationVariable String roomId, ChatMessage message) {
+        log.info(roomId + ": " + message);
+        messagingTemplate.convertAndSend("/topic/room." + roomId, message);
+    }
 
-        System.out.println("Sending message to room " + roomId + ": " + message);
-        messagingTemplate.convertAndSend("/topic/room" + roomId, message);
+    @MessageExceptionHandler
+    public void handleException(Throwable ex) {
+        log.error("Exception in STOMP handler", ex);
     }
 }
