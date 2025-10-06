372f128b1cbcc0d47c87c8762732ac2aac276294
백경석\seok <dno06117@naver.com>
2025-10-06 18:52:49 +0900

    ✨: Threadloacl에 샤드키 추가

---
+++ sharding/src/main/java/com/hellin22/sharding/entity/User.java
@@ -0,0 +1,33 @@
+package com.hellin22.sharding.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String name;
+
+    @Column(nullable = false, length = 100)
+    private String email;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+}
